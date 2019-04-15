5d115d92eeeb13b4754549fddb697055de880bd9
aleworkout <alegonznav.int@gmail.com>
2019-04-14 22:23:30 -0400

    se agrega parametro next ao middleware do server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-otylu.mongodb.net/t
 	useNewUrlParser: true
 });
 
-app.use((req, res) => {
+app.use((req, res, next) => {
 	req.io = io;
 
 	next();
